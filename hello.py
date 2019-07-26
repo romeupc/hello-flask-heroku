1aeca51d4fc6b410f66f259da314e3b754837f48
romeupc <48535364+romeupc@users.noreply.github.com>
2019-07-25 23:47:51 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, Response, render_template, url_for
+from datetime import datetime
+import time
 import requests
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    rsp = requests.get('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT').json()
-    return rsp['price']
+def time_feed():
+    def generate():
+        while True:
+            rsp = requests.get('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT').json()
+
+            yield str(rsp)
+            time.sleep(1)
+    return Response(generate())
 
 if __name__ == '__main__':
     app.run()
