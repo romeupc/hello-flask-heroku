e584ba44bd2d93e5aa4ed8bcf4a3453b10a7cd05
romeupc <48535364+romeupc@users.noreply.github.com>
2019-07-25 23:31:29 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Romeu'
+    rsp = requests.get('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT').json()
+    return rsp['price']
 
 if __name__ == '__main__':
     app.run()
